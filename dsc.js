ab95dde2a386341c550efb54cb3b9b3678a0e2fd
ulia <messageulia@gmail.com>
2023-07-18 17:20:19 +0200

    Update dsc.js

--- dsc.js
+++ dsc.js
@@ -5,26 +5,31 @@ async function fetchmd(arg, rm) {
     try {
         const { data } = await axios.get(arg[0]);
         const datasplit = data.split(/\r?\n/);
-        postmd(datasplit, arg, rm);
+        formatmessage(datasplit, arg, rm);
     } catch (err) {
         console.error(err);
         //rm.channel.send("error message lul");
     }
 }
 
-function postmd(data, arg, rm) {
-    let delay = 500;
+function formatmessage(data, arg, rm) {
+    let msgcache = '';
+    let delay = 300;
     let promise = Promise.resolve();
     data.forEach(function (d) {
         promise = promise.then(function () {
             if (d[0] == '!') {
                 const imagelink = d.split('![]').pop().slice(1,-1);
                 rm.channel.send({ files: [imagelink] });
+            } else if (d[0] == '|') {
+                rm.channel.send(msgcache);
+                msgcache = '';
+            } else if (d.length == 0) {
+                msgcache += '\n';
             } else {
-                if (d.length != 0) {
-                    rm.channel.send(d);
-                }
+                msgcache += d + '\n';
             }
+
             return new Promise(function (resolve) {
                 setTimeout(resolve, delay);
             });
